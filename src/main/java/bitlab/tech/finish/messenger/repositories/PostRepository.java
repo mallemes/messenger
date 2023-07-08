d9c8469c7837d13d2d2dab7990aa446653682da8
mallemes <dosaev.rysbek5@gmail.com>
2023-07-08 23:52:20 +0600

    changes

--- src/main/java/bitlab/tech/finish/messenger/repositories/PostRepository.java
+++ src/main/java/bitlab/tech/finish/messenger/repositories/PostRepository.java
@@ -3,15 +3,20 @@ package bitlab.tech.finish.messenger.repositories;
 import bitlab.tech.finish.messenger.models.Post;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
+@Transactional
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    public Post findPostById(Long id);
+     Post findPostById(Long id);
 
-    public void deletePostById(Long id);
+    @Modifying
+    @Query("DELETE FROM Post WHERE id = ?1")
+     void deletePostById(Long id);
 
 
 
